2e29beb5ac712b4c02dd4ccd0123fd5b93adb165
maxiVidarte <maxiavidarte@gmail.com>
2016-08-28 12:59:17 -0300

    Aplicacion 08
    Terminado

---
+++ Aplicacion08/Aplicacion.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Aplicacion 08</title>
+</head>
+<body>
+<?php 
+
+$v = array(1 =>90, 30=>7,'e'=>99,'hola'=>'mundo');
+
+foreach ($v as $key => $value) {
+
+echo "Indice: ".$key." , Valor: ".$value."<br>";
+}
+ ?>
+</body>
+</html>

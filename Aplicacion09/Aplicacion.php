6a401ca7caa395faf093d4ff1e31d3ec5d40c96a
maxiVidarte <maxiavidarte@gmail.com>
2016-08-28 13:01:50 -0300

    Aplicacion 09
    Terminado

---
+++ Aplicacion09/Aplicacion.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Aplicacion 09</title>
+</head>
+<body>
+<?php 
+
+$lapicera = array(
+	 array("Azul","pelican","Fino",50),
+	 array("Verde", "pelican","Grueso",50),
+	 array("Negro","pelican","Fino",60));
+
+echo "Color: ".$lapicera[0][0]." Marca:	".$lapicera[0][1]." Trazo: ".$lapicera[0][2]." Precio:	".$lapicera[0][3]."<br>";
+echo "Color: ".$lapicera[1][0]." Marca:	".$lapicera[1][1]." Trazo: ".$lapicera[1][2]." Precio: 	".$lapicera[1][3]."<br>";
+echo "Color: ".$lapicera[2][0]." Marca:	".$lapicera[2][1]." Trazo: ".$lapicera[2][2]." Precio:	".$lapicera[2][3]."<br>";
+
+
+ ?>
+</body>
+</html>

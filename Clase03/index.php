81923c680494c026e38d445ffb0ad875d8a7a6b7
maxiVidarte <maxiavidarte@gmail.com>
2016-09-05 19:47:14 -0300

    Clase 3 incluir formulario por defecto y primer paso

--- Clase03/index.php
+++ Clase03/index.php
@@ -6,11 +6,24 @@
 <body>
 	<form>
 		<label>Nombre</label>
-	<input type="text" name= "nombre" placeholder="hola mundo">
+	<input type="text" name= "nombre" placeholder="hola">
+	<input type= "text" name="numerouno">
 	<input type= "submit">
 
 
 	</form> 
 </body>
 </html>
+<?php 
+if(isset($_GET['nombre']))
+{
+	var_dump($_GET);
+	echo "<br>".$_GET['nombre'];
+}
+else
+{
+
+	echo "primer ingreso";
+}
+ ?>
 

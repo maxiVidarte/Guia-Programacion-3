d3302a5676e741a992a7712cf72a58a046bdd130
maxiVidarte <maxiavidarte@gmail.com>
2016-09-19 20:18:43 -0300

    mejoro el ejercicio 40

--- Aplicacion40/index.php
+++ Aplicacion40/index.php
@@ -4,8 +4,10 @@
 </head>
 <body>
 	<form enctype= "multipart/form-data" method = "post" action="nexo.php">
-		<input type="file" name="foto" >
-		<input type= "submit" name="estaOk" value="Aceptar">
+		<input type="file" name="foto" />
+		<input type= "image"/ >
+		<input type= "submit" name="estaOk" value="Aceptar"/>
+		
 	</form>
 
 </body>
--- Aplicacion40/nexo.php
+++ Aplicacion40/nexo.php
@@ -1,8 +1,18 @@
 <?php 
-var_dump($_FILES);
-if (!isset($_FILES["foto"])) 
+/*var_dump($_FILES);*/
+if (isset($_FILES["foto"])) 
 {
-echo "hola";
-
+	$NombreCompleto=explode(".", $_FILES['foto']['name']);
+	$Extension=  end($NombreCompleto);
+	$destino = "fotos/".$_FILES["foto"]["name"];
+	$foto=".".$Extension;
+	var_dump($destino);
+	//echo $Extension;
+	//echo $destino;
+	//echo $foto;
+	if(move_uploaded_file($_FILES["foto"]["tmp_name"],$destino))
+	{
+		echo "hola guardo el archivo";
+	}
 }
  ?>
